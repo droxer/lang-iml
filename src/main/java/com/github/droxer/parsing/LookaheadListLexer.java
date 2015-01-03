75f53cabd5dd22105c24db9134a6856954eb6b52
He Fei <droxer.he@gmail.com>
2015-01-03 16:32:20 +0800

    remove unused code.

--- src/main/java/com/github/droxer/parsing/LookaheadListLexer.java
+++ src/main/java/com/github/droxer/parsing/LookaheadListLexer.java
@@ -2,70 +2,82 @@ package com.github.droxer.parsing;
 
 public class LookaheadListLexer {
 
-    private static final char EOF = (char)-1;
+    private static final char EOF = (char) -1;
     private static final int EOF_TYPE = -1;
+
+    public static int NAME = 2;
+
+    public static int COMMA = 3;
+    public static int LBRACK = 4;
+    public static int RBRACK = 5;
+    public static int EQUALS = 6;
+    public static String[] tokenNames = {"n/a", "<EOF>", "NAME", "COMMA", "LBRACK", "RBRACK", "EQUALS"};
 
     private String input;
     private int p;
     private char c;
 
-    public static int NAME = 2;
-    public static int COMMA = 3;
-    public static int LBRACK = 4;
-    public static int RBRACK = 5;
-    public static int EQUALS = 6;
-
-    public static String[] tokenNames = {"n/a", "<EOF>", "NAME", "COMMA", "LBRACK", "RBRACK", "EQUALS"};
-
-    public LookaheadListLexer(String input){
+    public LookaheadListLexer(String input) {
         this.input = input;
         c = input.charAt(p);
     }
 
-    public String getTokenName(int x){
+    public String getTokenName(int x) {
         return tokenNames[x];
     }
 
     public Token nextToken() {
-        while (c != EOF){
-            switch ( c ){
-                case ' ':case '\t':case '\r':ws(); continue;
-                case ',': consume(); return new Token(COMMA, ",");
-                case '[': consume(); return new Token(LBRACK, "[");
-                case ']': consume(); return new Token(RBRACK, "]");
-                case '=': consume(); return new Token(EQUALS, "=");
+        while (c != EOF) {
+            switch (c) {
+                case ' ':
+                case '\t':
+                case '\r':
+                    ws();
+                    continue;
+                case ',':
+                    consume();
+                    return new Token(COMMA, ",");
+                case '[':
+                    consume();
+                    return new Token(LBRACK, "[");
+                case ']':
+                    consume();
+                    return new Token(RBRACK, "]");
+                case '=':
+                    consume();
+                    return new Token(EQUALS, "=");
                 default:
-                    if ( isLetter()){
+                    if (isLetter()) {
                         return name();
                     }
-                    throw new Error("invalid character: "+ c);
+                    throw new Error("invalid character: " + c);
             }
 
         }
         return new Token(EOF_TYPE, "<EOF>");
     }
 
-    private void consume(){
-        p++ ;
-        if(p >= input.length()){
+    private void consume() {
+        p++;
+        if (p >= input.length()) {
             c = EOF;
-        }else{
+        } else {
             c = input.charAt(p);
         }
     }
 
     private Token name() {
         StringBuilder builder = new StringBuilder();
-        do{
-          builder.append(c);
+        do {
+            builder.append(c);
             consume();
-        }while (isLetter());
+        } while (isLetter());
 
         return new Token(NAME, builder.toString());
     }
 
     private void ws() {
-        while (c == ' ' || c == '\t' || c == '\n' || c == '\r'){
+        while (c == ' ' || c == '\t' || c == '\n' || c == '\r') {
             consume();
         }
     }
